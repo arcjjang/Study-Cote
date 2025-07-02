d12e0bf5adc362f6fe44d24875824403c6b76729
arcjjang <arcelias@naver.com>
2025-07-02 18:18:22 +0900

    [제목] 그리디 학습 [내용] - 그리디 문제 풀이2

--- src/main/java/this_is_cote/GreedyProblems.java
+++ src/main/java/this_is_cote/GreedyProblems.java
@@ -1,8 +1,10 @@
 package this_is_cote;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.ConcurrentNavigableMap;
 
 public interface GreedyProblems {
@@ -139,12 +141,17 @@ public interface GreedyProblems {
 
     }
 
-    class GreedyProblemMain11_05 {
+    class GreedyProblemMain11_06 {
         public static void main(String[] args) {
             int[] foodTimes = {3, 1, 2};
             int k = 5;
             int result = solution(foodTimes, k);
             System.out.println("result = " + result);   // 1
+
+            int[] foodTimes2 = {3, 5, 1, 6, 5, 3};
+            int k2 = 20;
+            int result2 = solution2(foodTimes2, k2);
+            System.out.println("result2 = " + result2); // 4
         }
 
         public static int solution(int[] food_times, long k) {
@@ -152,16 +159,16 @@ public interface GreedyProblems {
             int count = 0;
             while (true) {
                 if (count == k + 1) {
-                    System.out.println("curr = " + curr);
+//                    System.out.println("curr = " + curr);
                     break;
                 }
 
-                System.out.println("index = " + index);
+//                System.out.println("index = " + index);
                 if (food_times[index] > 0) {
                     food_times[index]--;
                     count++;
                     curr = index;
-                    System.out.println(Arrays.toString(food_times));
+//                    System.out.println(Arrays.toString(food_times));
                 }
                 if (index == food_times.length - 1) {
                     index = 0;
@@ -171,6 +178,56 @@ public interface GreedyProblems {
             }
 
             return curr + 1;
+        }
+
+        static class Food {
+            int time;
+            int idx;
+
+            public Food(int time, int idx) {
+                this.time = time;
+                this.idx = idx;
+            }
+
+            @Override
+            public String toString() {
+                return "Food{" +
+                        "time=" + time +
+                        ", idx=" + idx +
+                        '}';
+            }
+        }
+
+        public static int solution2(int[] food_times, int k) {
+            List<Food> foods = new ArrayList<>();
+            int n = food_times.length;
+            for (int i = 0; i < n; i++) {
+                foods.add(new Food(food_times[i], i + 1));
+            }
+            foods.sort(Comparator.comparingInt(o -> o.time));
+            System.out.println(foods);
+            int preTime = 0;
+            int idx = 0;
+            for (Food food : foods) {
+                int diff = food.time - preTime;
+                if (diff != 0) {
+                    int spend = diff * n;
+                    if (spend <= k) {
+                        k -= spend;
+                        preTime = food.time;
+                    } else {
+                        k %= n;
+                        System.out.println("idx = " + idx);
+                        foods.subList(idx, food_times.length).sort(Comparator.comparingInt(o -> o.idx));
+                        System.out.println("foods = " + foods);
+                        return foods.get(idx + k).idx;
+                    }
+                }
+                idx++;
+                n--;
+            }
+
+            return -1;
         }
     }
 
